5c215f7810b59e6ef2d209d12ad5bf031e4f6ced
Buhobyte <byron.lopez.begins@gmail.com>
2023-02-05 04:10:44 -0500

    mega load

--- 1_Empezando/4_mega_load.js
+++ 1_Empezando/4_mega_load.js
@@ -3,9 +3,9 @@ import { check, sleep } from 'k6';
 
 export const options = {
     stages: [
-        { duration: '2m', target: 4000 },
         { duration: '1m', target: 4000 },
-        { duration: '2m', target: 0 },
+        { duration: '1m', target: 4000 },
+        { duration: '1m', target: 0 },
     ],
 };
 
